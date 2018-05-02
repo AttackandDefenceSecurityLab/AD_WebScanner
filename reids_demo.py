7ff65a4fa890832c9afc0a3f6df9becf0b20085d
hjc981024 <450342251@qq.com>
2018-05-03 00:15:07 +0800

    根据开发约束修改

--- reids_demo.py
+++ reids_demo.py
@@ -1,5 +1,5 @@
-﻿import redis
-
+#Author:Chernobyl   2018/5/3
+import redis
 
 save_pool = redis.ConnectionPool(host='127.0.0.1', port=6379, decode_responses=True)#开启本地radis
 test1 = redis.Redis(connection_pool=save_pool)#创建一个连接实例
