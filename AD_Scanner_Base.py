efdb6923d656f4f6a086aba53f4717fd111d2b5d
hjc981024 <450342251@qq.com>
2018-05-02 12:46:47 +0800

    与url_spider模块对接
    新增参数 --spider_threads 设置爬虫线程数

--- AD_Scanner_Base.py
+++ AD_Scanner_Base.py
@@ -1,4 +1,4 @@
-#import url_spider
+from url_spider import SpiderMain
 import re
 import os
 import sys
@@ -12,10 +12,10 @@ def terminal_input():
         -u/--url= : 传入的URL
         -h ：帮助
     '''
-
+    ter_opt={}
     url = ''
     try:
-        opts,args = getopt.getopt(sys.argv[1:],"hu:",['url='])
+        opts,args = getopt.getopt(sys.argv[1:],"hu:",['url=','spider-threads='])
     except getopt.GetoptError:
       print("Command Error, type -h for usage")
       sys.exit(2)
@@ -27,11 +27,13 @@ def terminal_input():
             print("Usage:")
             print("-u/--url= : URL for scanning")
             print("-h : help")
+            print('--spider-thread= : Threads num of url_spider module')
             sys.exit(0)
         elif opt in ('-u','--url'):
-            url = arg
-
-    return url
+            ter_opt['url'] = arg
+        elif opt in ('--spider-threads'):
+            ter_opt['spider_threads'] = arg
+    return ter_opt
 
 class base:
     '''
@@ -68,16 +70,30 @@ class base:
             print('URL Type Error!')
             sys.exit(1)#URL_ERROR
 
+    def opt_handler(self):
+        '''命令行参数处理
+        针对各模块特有设置项，设置相应键值对，用于初始化和存储信息
+        '''
+
+        '''url_spider'''
+        if 'spider_threads' in self.info.keys():
+            self.base_linker.hset('input_opt','spider_threads',self.info['spider_threads'])
+            self.spider_threads = self.info['spider_threads']
+        else:
+            self.spider_threads = 100
+            self.base_linker.hset('input_opt','spider_threads',self.info['spider_threads'])
 
     def __init__(self):
-        self.url = terminal_input()
+        self.info = terminal_input()
+        self.url = self.info['url']
         self.save_pool = redis.ConnectionPool(host='127.0.0.1', port=6379, decode_responses=True)#开启本地radis
         self.base_linker = redis.Redis(connection_pool=self.save_pool)
         self.url_check(self.url)
-        print('URL:'+self.base_linker.hget('input_url','url')+'   URL_Type:'+str(self.base_linker.hget('input_url','url_type')))
+        self.opt_handler()
+        print('URL:'+self.base_linker.hget('input_url','url')+'   URL_Type:'+str(self.base_linker.hget('input_url','url_type')\
+        +'   Spider_threads : '+str(self.base_linker.hget('input_opt','spider_threads'))))
         '''各模块初始化'''
-        #self.SpiderMain(self.url, 100)
-        #...
+        self.Spider = SpiderMain(self.url, self.spider_threads)
 
     def module_check(self):
         '''模块状态检查'''
