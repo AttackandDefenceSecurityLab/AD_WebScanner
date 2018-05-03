2910bcbbe045e14f2d8a20573c37af4e00bbb52c
nxnx <540026837@qq.com>
2018-05-03 12:39:03 +0800

    更新了注释
    更新了注释，把url改为传参

--- Burp_force_directory.py
+++ Burp_force_directory.py
@@ -2,13 +2,16 @@ import requests
 import threading
 
 class Scanner():
-    def __init__(self):
-        self.url = input("请输入要检测的网址(包括http://或https://)：")
+    def __init__(self, url):
+        self.url = url
         self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                                       'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36'}
         self.doc_list = ['ASP.txt','ASPX.txt','DIR.txt','JSP.txt','MDB.txt','PHP.txt']
 
     def more_threads(self):
+        '''
+        为每个字典创建一个线程
+        '''
         threads = []
         for k in range(0,len(self.doc_list)):
             print(self.doc_list[k])
@@ -23,6 +26,9 @@ class Scanner():
 
 
     def combine_url(self,doc_name):
+        '''
+        从字典中逐行取出子目录，并将其与传入的网址组合
+        '''
         #doc_name = 'ASP.txt'
         print(doc_name)
         with open(r'dictionary\\'+doc_name) as file_obj:
@@ -32,6 +38,9 @@ class Scanner():
                 self.judge(test_url.rstrip())
 
     def judge(self, test_url):
+        '''
+        判断所传入的连接是否存在
+        '''
         try:
             #print(test_url)
             k = self.request(test_url)
@@ -49,5 +58,6 @@ class Scanner():
         r = requests.head(test_url, headers=self.headers)
         return r
 
-Web_scanner = Scanner()
+url = 'www.sdlongli.com'
+Web_scanner = Scanner(url)
 Web_scanner.more_threads()
