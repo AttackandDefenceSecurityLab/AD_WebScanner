d1961b1d44c4d69788f2ca5d7238fc6158c4512a
nxnx <540026837@qq.com>
2018-05-06 22:19:57 +0800

    添加了注释
    尝试连接到redis

--- Burp_force_directory.py
+++ Burp_force_directory.py
@@ -1,12 +1,17 @@
+#Burp_force_directory
 import requests
 import threading
+import redis
 
 class Scanner():
-    def __init__(self, url):
+    def __init__(self, save_pool, url):
+        self.module_redis = redis.Redis(connection_pool=save_pool)
         self.url = url
         self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                                       'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36'}
         self.doc_list = ['ASP.txt','ASPX.txt','DIR.txt','JSP.txt','MDB.txt','PHP.txt']
+        #获取到的所存在的url
+        self.get_url = []
 
     def more_threads(self):
         '''
@@ -47,7 +52,10 @@ class Scanner():
             #print(k.status_code)
             if k.status_code == 200:
                 print(test_url)
+                self.get_url.append(test_url)
         except Exception as e:
+            #pass
+            #测试模式下开启报错输出
             print(e)
 
     def request(self, test_url):
@@ -55,9 +63,15 @@ class Scanner():
         用get方法会请求整个【头部+正文】，浪费资源
         利用head方法，只请求【资源头部】
         '''
-        r = requests.head(test_url, headers=self.headers)
+        r = requests.head(test_url, headers=self.headers, timeout=3)
         return r
 
-url = 'www.sdlongli.com'
-Web_scanner = Scanner(url)
-Web_scanner.more_threads()
+    def print_get_url(self):
+        self.print_get_url = set(self.print_get_url)
+        print(self.get_url)
+
+#url = 'https://www.andseclab.cn'
+save_pool = redis.ConnectionPool(host='127.0.0.1', port=6379, decode_responses=True)#开启本地radis
+#Web_scanner = Scanner(url,save_pool)
+#Web_scanner.more_threads()
+#Web_scanner.print_get_url()
